d9e09c9c03404656e910484040226f97071f8e6f
Bohdan <example@gmail.com>
2023-03-22 15:25:41 +0100

    first

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	
+	"overrides": [
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//alert 'Hello'
+// const result = confirm("Are u here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+const answers = [] ;
+
+// answers[0] = prompt("Ваше имя?" , "") ;
+// answers[1] = prompt("Ваша фамилия" , "") ;
+// answers[2] = prompt("Ваше Дата рождения" , "") ;
+
+// console.log	(typeof(answers));
+
+
+// const category = "toys" ;
+// console.log(`https://someurl.com/${category}/5`)
+
+// const user = "ivan";
+// alert(`Hello, ${user}`);
+console.log(2*4 === 8);
+
+const isChecked = false,
+	isClose = false;
+
+
+console.log(isChecked || !isClose);
